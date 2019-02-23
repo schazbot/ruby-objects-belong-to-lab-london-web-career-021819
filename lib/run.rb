d4ce61beb05d7ac1a3bde9e964e84c0ffe184a25
github username <github email address>
2019-02-23 07:58:06 +0000

    Done.

---
+++ lib/run.rb
@@ -0,0 +1,16 @@
+require_relative "./artist.rb"
+require_relative "./song.rb"
+require 'pry'
+
+#artists
+a1 = Artist.new("Janelle Monae")
+a2 = Artist.new("Grimes")
+#
+# #song
+a1 = Song.new("Pink", "Janelle Monae")
+a2 = Song.new("Smiling and acting normal", "Grimes")
+a3 = Song.new("Sin", "NIN")
+
+
+binding.pry
+"aklsjdhfksjdhfklasjf"
